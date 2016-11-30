f0539f63d9959cc3d2cd990ca07761f355a4afbc
Carlos Coves Prieto <escorponox@gmail.com>
2016-11-30 13:12:46 +0100

    all test pass

--- app/scripts/link-harvester.js
+++ app/scripts/link-harvester.js
@@ -1,3 +1,25 @@
-const linkHarvester = () => {};
+const linkHarvester = (input = '') => {
+  const regExp = /<a [^>]*(href="((mailto:|https?:\/\/)([^"]*)))"[^>]*>([^<]*)<\/a>/gi;
+  const rawHarvest = [];
+  let execResult;
+  while ((execResult = regExp.exec(input)) !== null) {
+    rawHarvest.push(execResult);
+  }
 
-export default harvester;
+  return rawHarvest.reduce((acc, curr) => {
+    if (curr[3] === 'mailto:') {
+      acc.emailAddresses = acc.emailAddresses || [];
+      acc.emailAddresses.push(curr[4]);
+    }
+    if (/https?:\/\//.test(curr[3])) {
+      acc.links = acc.links || [];
+      acc.links.push({
+        linkText: curr[5],
+        url: curr[2]
+      });
+    }
+    return acc;
+  }, {});
+};
+
+export default linkHarvester;
